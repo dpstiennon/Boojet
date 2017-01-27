7fd386b5094faeec3385a68c634b62c3f3f4519c
David Stiennon <david.stiennon@gmail.com>
2017-01-27 15:15:50 -0600

    Calendar forward-back navigation working

--- Boojet/Scripts/Controllers/calendarController.js
+++ Boojet/Scripts/Controllers/calendarController.js
@@ -1,24 +1,55 @@
 ﻿(function() {
     var calendarController = function() {
-        this.currentDate = new CalDate(this.month, this.year);
+        this.year = Number(this.year);
+        this.month = Number(this.month);
+        this.currentDate = new Date(this.year, this.month - 1);
         this.monthsToShow = 12;
-        this.dates = this.buildMonths();
+        this.dates = this.buildDateList();
     };
 
-    calendarController.prototype.buildMonths = function buildMonths(month, year) {
-        var today = new CalDate(new Date().getFullYear(), new Date().getMonth());
+    calendarController.prototype.buildDateList = function() {
         var months = [this.currentDate];
-        var next = this.currentDate.next();
-        while (!today.equals(next) && months.length <= 6 ) {
-            next = next.next();
+        var now = new Date();
+        var next = this.nextMonth(this.currentDate);
+        while (next < now && months.length < 6) {
             months.push(next);
+            next = this.nextMonth(next);
         }
-        var previous = this.currentDate.previous();
-        while (months.length <= 12) {
+        var previous = this.previousMonth(this.currentDate);
+        while (months.length < 12) {
             months.unshift(previous);
-            previous = previous.previous();
+            previous = this.previousMonth(previous);
         }
         return months;
+    };
+
+    calendarController.prototype.scrollRight = function() {
+        var lastDate = this.dates.slice(-1).pop();
+        this.dates.push(this.nextMonth(lastDate));
+        this.dates.shift();
+    };
+
+    calendarController.prototype.scrollLeft = function() {
+        var firstDate = this.dates[0];
+        this.dates.unshift(this.previousMonth(firstDate));
+        this.dates.pop();
+
+    };
+
+    calendarController.prototype.nextMonth = function(startDate) {
+        var next = new Date(startDate);
+        next.setMonth(next.getMonth() + 1);
+        return next;
+    };
+
+    calendarController.prototype.previousMonth = function(startDate) {
+        var next = new Date(startDate);
+        next.setMonth(next.getMonth() - 1);
+        return next;
+    };
+
+    calendarController.prototype.monthString = function(myDate) {
+        return myDate.toLocaleString('en-us', { month: "short" });
     }
 
     angular.module('boojet')
