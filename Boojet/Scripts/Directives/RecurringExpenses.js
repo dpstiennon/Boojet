033a909308de2496abc1ab6b9add95685fe1cd2f
David Stiennon <david.stiennon@gmail.com>
2017-01-27 18:54:57 -0600

    pulled recurring expenses out into directive

---
+++ Boojet/Scripts/Directives/RecurringExpenses.js
@@ -0,0 +1,11 @@
+﻿angular.module('boojet')
+    .directive('recurringExpenses',
+        function() {
+            return {
+                restrict: 'E',
+                templateUrl: '/Scripts/Templates/RecurringExpenses.html',
+                controller: 'RecurringController',
+                controllerAs: 'recurringCtrl',
+                replace: true
+            };
+        });
